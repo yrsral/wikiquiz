5933bbc170af8151aa0f30255306af748b9d5d76
Euan Ong <euan.l.y.ong@gmail.com>
2014-07-28 19:17:42 +0100

    Bugfix

--- WIkiQuiz/src/com/homegrownapps/wikiquiz/MainActivity.java
+++ WIkiQuiz/src/com/homegrownapps/wikiquiz/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void onClickA(View v){
+		
+	}
+	
+	public void onClickB(View v){
+		
+	}
+	
+	public void onClickC(View v){
+		
 	}
 }
