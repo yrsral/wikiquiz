d99e439c459a8d008d10b4632f48007616cb141b
Euan Ong <euan.l.y.ong@gmail.com>
2014-07-30 09:38:20 +0100

    A bit more work

--- WikiQuiz/src/wikiParse/QuestionFinder.java
+++ WikiQuiz/src/wikiParse/QuestionFinder.java
@@ -2,13 +2,15 @@ package wikiParse;
 
 public class QuestionFinder {
 	public String[] questionParse(String article, int question){
-		String article_new = article.toLowerCase();
+		String article_new = article.replaceAll("\n", "");
+		//.toLowerCase();
+		
 		int index = 0;
 		int seek = 0;
 		for (int i = 0; i<question+1; i++){
 			//''' signifies bold in WikiMarkup - the markup language of wikipedia
 			index = article_new.indexOf("'''", seek);
-			seek = index;
+			seek = index+2;
 			seek = article_new.indexOf("'''", seek);
 		}
 		char character = 'a';
@@ -18,7 +20,10 @@ public class QuestionFinder {
 			seek_number = seek_number - 1;
 			//going back through the numbers to find the beginning of the sentence
 		}
-		//TODO: loop back through chars to find full stop, cater for '/n's
+		
+
+		
+		//TODO: loop back through chars to find full stop, cater for '\n's
 		//TODO: find conjugation of 'to be'
 		//TODO: return subject, object (not null!)
 		return null;
