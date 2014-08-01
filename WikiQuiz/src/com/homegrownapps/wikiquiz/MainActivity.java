8858afbc053fd06fd1b1aa720d9a1e86ef3089f3
Euan Ong <euan.l.y.ong@gmail.com>
2014-08-01 19:17:52 +0100

    Button colours!

--- WikiQuiz/src/com/homegrownapps/wikiquiz/MainActivity.java
+++ WikiQuiz/src/com/homegrownapps/wikiquiz/MainActivity.java
@@ -1,5 +1,9 @@
 package com.homegrownapps.wikiquiz;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import wikiParse.QuestionCreator;
@@ -9,12 +13,16 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 // TODO: Parse for wikt:| in used links
@@ -25,6 +33,7 @@ public class MainActivity extends Activity {
 		public static String topic;
 		public static Context context;
 		public static int button;
+		public static int progress;
 	}
 	
 	private ProgressDialog pdz;
@@ -50,8 +59,14 @@ public class MainActivity extends Activity {
 			if (unparsed != "none"){
 				QuestionFinder qf = new QuestionFinder();
 				String[] parsed = qf.sentenceParse(unparsed);
+				List<String> parse = Arrays.asList(parsed);
+				Collections.shuffle(parse);
+				parsed = parse.toArray(parsed);
+				//Random rand = new Random();
+				//int question_no = rand.nextInt(parsed.length-1)+1;
 				String[] subobjverb = qf.questionParse(parsed, 1);
 				//possibly random numbers for question number argument in questionParse
+				//uncapitalise 'a' or 'an'
 				QuestionCreator qc = new QuestionCreator();
 				String question = qc.questionMake(subobjverb[0], subobjverb[1], subobjverb[2]);
 				String[] qa = {question, subobjverb[3]};
@@ -65,7 +80,14 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPostExecute(String[] result) {
 			if (pdz != null) {
-				pdz.dismiss();
+				try {
+					if (pdz.isShowing()) {
+				        pdz.dismiss();       
+				    }
+		            pdz = null;
+		          } catch (Exception e) {
+		         // nothing
+		          }
 			}
 			TextView tv = (TextView)findViewById(R.id.textView2);
 			tv.setText(result[0]);
@@ -107,20 +129,104 @@ public class MainActivity extends Activity {
 	// get ticks and crosses
 	
 	public void onClickA(View v){
+		Button a = (Button)findViewById(R.id.button1);
+		Button b = (Button)findViewById(R.id.button2);
+		Button c = (Button)findViewById(R.id.button3);
+		a.setEnabled(false);
+		a.invalidate();
+		b.setEnabled(false);
+		b.invalidate();
+		c.setEnabled(false);
+		c.invalidate();
 		if (Global.button == 0){
-			
+			Button b0 = (Button)findViewById(R.id.button1);
+			b0.setBackgroundColor(Color.GREEN);
+			b0.invalidate();
+		} else {
+			Button b_origin = (Button)findViewById(R.id.button1);
+			b_origin.setBackgroundColor(Color.GREEN);
+			b_origin.invalidate();
+			if (Global.button == 0){
+				Button b0 = (Button)findViewById(R.id.button1);
+				b0.setBackgroundColor(Color.GREEN);
+				b0.invalidate();
+			} else if (Global.button == 1) {
+				Button b1 = (Button)findViewById(R.id.button2);
+				b1.setBackgroundColor(Color.GREEN);
+				b1.invalidate();
+			} else{
+				Button b2 = (Button)findViewById(R.id.button3);
+				b2.setBackgroundColor(Color.GREEN);
+				b2.invalidate();
+			}
 		}
 	}
 	
 	public void onClickB(View v){
+		Button a = (Button)findViewById(R.id.button1);
+		Button b = (Button)findViewById(R.id.button2);
+		Button c = (Button)findViewById(R.id.button3);
+		a.setEnabled(false);
+		a.invalidate();
+		b.setEnabled(false);
+		b.invalidate();
+		c.setEnabled(false);
+		c.invalidate();
 		if (Global.button == 1){
-			
+			Button b1 = (Button)findViewById(R.id.button2);
+			b1.setBackgroundColor(Color.GREEN);
+			b1.invalidate();
+		} else {
+			Button b_origin = (Button)findViewById(R.id.button2);
+			b_origin.setBackgroundColor(Color.RED);
+			b_origin.invalidate();
+			if (Global.button == 0){
+				Button b0 = (Button)findViewById(R.id.button1);
+				b0.setBackgroundColor(Color.GREEN);
+				b0.invalidate();
+			} else if (Global.button == 1) {
+				Button b1 = (Button)findViewById(R.id.button2);
+				b1.setBackgroundColor(Color.GREEN);
+				b1.invalidate();
+			} else{
+				Button b2 = (Button)findViewById(R.id.button3);
+				b2.setBackgroundColor(Color.GREEN);
+				b2.invalidate();
+			}
 		}
 	}
 	
 	public void onClickC(View v){
+		Button a = (Button)findViewById(R.id.button1);
+		Button b = (Button)findViewById(R.id.button2);
+		Button c = (Button)findViewById(R.id.button3);
+		a.setEnabled(false);
+		a.invalidate();
+		b.setEnabled(false);
+		b.invalidate();
+		c.setEnabled(false);
+		c.invalidate();
 		if (Global.button == 2){
-			
+			Button b2 = (Button)findViewById(R.id.button3);
+			b2.setBackgroundColor(Color.GREEN);
+			b2.invalidate();
+		}else {
+			Button b_origin = (Button)findViewById(R.id.button3);
+			b_origin.setBackgroundColor(Color.RED);
+			b_origin.invalidate();
+			if (Global.button == 0){
+				Button b0 = (Button)findViewById(R.id.button1);
+				b0.setBackgroundColor(Color.GREEN);
+				b0.invalidate();
+			} else if (Global.button == 1) {
+				Button b1 = (Button)findViewById(R.id.button2);
+				b1.setBackgroundColor(Color.GREEN);
+				b1.invalidate();
+			} else{
+				Button b2 = (Button)findViewById(R.id.button3);
+				b2.setBackgroundColor(Color.GREEN);
+				b2.invalidate();
+			}
 		}
 	}
 }
